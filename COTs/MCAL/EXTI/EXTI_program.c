b39d616b3faaaa10fcc8bab522ae6992e7023008
Kerolos Hanna <Kerolos.hanna@hotmail.com>
2023-07-01 14:42:22 +0300

    Building the External Interrupts Driver

---
+++ COTs/MCAL/EXTI/EXTI_program.c
@@ -0,0 +1,29 @@
+/*****************************************************************/
+/*****************************************************************/
+/************		Author: Kerolos Hanna			**************/
+/************		Kerolos.Hanna@hotmail.com		**************/
+/************		Layer:	MCAL					**************/
+/************		SWC:	EXTI						**************/
+/************		version: 1.00					**************/
+/*****************************************************************/
+/*****************************************************************/
+
+#include "..\..\LIB\STD_TYPES.h"
+#include "..\..\LIB\BIT_MATH.h"
+#include "..\..\MCAL\DIO\DIO_interface.h"
+
+#include "EXTI_register.h"
+#include "EXTI_config.h"
+#include "EXTI_private.h"
+#include "EXTI_interface.h"
+
+
+void EXTI_vdInt0Init(){
+
+}
+void EXTI_vdInt1Init(){
+
+}
+void EXTI_vdInt2Init(){
+
+}
