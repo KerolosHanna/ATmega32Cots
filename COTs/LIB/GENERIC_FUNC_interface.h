db492af44b2841963a7f437b13500cd9c92a959b
Kerolos Hanna <Kerolos.hanna@hotmail.com>
2023-11-29 03:41:18 +0200

    Creating Generic Function (Mapping)

---
+++ COTs/LIB/GENERIC_FUNC_interface.h
@@ -0,0 +1,15 @@
+/*****************************************************************/
+/************		Author: Kerolos Hanna"			**************/
+/************		Kerolos.Hanna@hotmail.com		**************/
+/************		Layer:	MCAL					**************/
+/************		SWC:	GENERIC_FUNC			**************/
+/************		version: 1.00					**************/
+/*****************************************************************/
+/*****************************************************************/
+
+#ifndef GENERIC_FUNC_INTERFACE_H
+#define GENERIC_FUNC_INTERFACE_H
+
+suint32 s32Mapping(suint32 Copy_s23MinInput, suint32 Copy_s23MaxInput, suint32 Copy_s23MinOutput, suint32 Copy_s23MaxOutput, suint32 Copy_s23InputValue);
+
+#endif
---
+++ COTs/LIB/GENERIC_FUNC_program.c
@@ -0,0 +1,21 @@
+/*****************************************************************/
+/************		Author: Kerolos Hanna"			**************/
+/************		Kerolos.Hanna@hotmail.com		**************/
+/************		Layer:	MCAL					**************/
+/************		SWC:	GENERIC_FUNC			**************/
+/************		version: 1.00					**************/
+/*****************************************************************/
+/*****************************************************************/
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+
+#include "GENERIC_FUNC_interface.h"
+
+
+suint32 s32Mapping(suint32 Copy_s23MinInput, suint32 Copy_s23MaxInput, suint32 Copy_s23MinOutput, suint32 Copy_s23MaxOutput, suint32 Copy_s23InputValue){
+	suint32 Local_s32OutputValue;
+
+	Local_s32OutputValue = Copy_s23MaxOutput - (((Copy_s23MaxInput - Copy_s23InputValue)*(Copy_s23MaxOutput-Copy_s23MinOutput))/(Copy_s23MaxInput - Copy_s23MinInput));
+	return Local_s32OutputValue;
+}
